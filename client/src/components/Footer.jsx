aa18bec3edcaf91ac36b2716673d1ecf1f69464e
Madhura Ingole <ingolemadhura1611@gmail.com>
2024-03-05 04:00:47 +0530

    text update

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -12,7 +12,7 @@ export default function FooterCom() {
               className='self-center whitespace-nowrap text-lg sm:text-xl font-semibold dark:text-white'
             >
               <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
-                Unity
+                Madhura's
               </span>
               Blog
             </Link>
@@ -69,7 +69,7 @@ export default function FooterCom() {
         <div className='w-full sm:flex sm:items-center sm:justify-between'>
           <Footer.Copyright
             href='#'
-            by="Unity blog"
+            by="Madhura's blog"
             year={new Date().getFullYear()}
           />
           <div className="flex gap-6 sm:mt-0 mt-4 sm:justify-center">
