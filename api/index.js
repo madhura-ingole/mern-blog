7fc404ab1c81be3113d2c2cbcf0e6929b16ae5d9
Madhura Ingole <ingolemadhura1611@gmail.com>
2024-03-04 23:58:30 +0530

    add create a post api route

--- api/index.js
+++ api/index.js
@@ -3,37 +3,39 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
+import postRoutes from './routes/post.route.js';
 import cookieParser from 'cookie-parser';
 
 dotenv.config();
 
 mongoose
-    .connect(process.env.MONGO)
-    .then(() =>{ 
-        console.log('MongoDb is connected');
-    })
-    .catch((err) => {
-        console.log(err);
-    });
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log('MongoDb is connected');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-const app=express();
+const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
 
-app.listen(3000, ()=>{
-    console.log('Server is running n port 3000!!!');
+app.listen(3000, () => {
+  console.log('Server is running on port 3000!');
 });
 
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
+app.use('/api/post', postRoutes);
 
-app.use((err,req,res,next) => {
-    const statusCode=err.statusCode || 500;
-    const message=err.message || 'Internal Server Error';
-    res.status(statusCode).json({
-        success: false,
-        statusCode,
-        message
-    })
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
 });
